28005c0283291d4ebb98f53a4abbf6c80e22f890
terracat474 <94134200+terracat474@users.noreply.github.com>
2022-06-04 16:31:57 +0300

    Create sh_rollingtable.lua

---
+++ sh_rollingtable.lua
@@ -0,0 +1,9 @@
+local BENCH = {}
+
+BENCH.Class = "weed"
+BENCH.Name = "Rolling Table"
+BENCH.Desc = "Can be used to roll blunts."
+BENCH.Model = "models/mosi/fallout4/furniture/workstations/workshopbench.mdl"
+BENCH.Illegal = true
+
+impulse.RegisterBench(BENCH)
