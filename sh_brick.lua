742cc58cdc1139667768f9f5ca3c93852419d97f
terracat474 <94134200+terracat474@users.noreply.github.com>
2023-04-11 13:53:14 +0300

    Create sh_brick.lua

---
+++ sh_brick.lua
@@ -0,0 +1,9 @@
+local BENCH = {}
+
+BENCH.Class = "hand"
+BENCH.Name = "A Brick"
+BENCH.Desc = "For quickly making stuff on the go."
+BENCH.Model = "models/mosi/fallout4/furniture/workstations/workshopbench.mdl"
+BENCH.Illegal = true
+
+impulse.RegisterBench(BENCH)
